4e4e6be7f998b7a2d24998a00c7cf955e30da3b7
Quentin Gouazé <quentin.gouaze@campus-igs-toulouse.fr>
2020-05-07 16:20:08 +0200

    Coordonnées fonctionelles. Ajout début vérification si bateau se trouve dans battlefield (start Position seulement pour le moment)

--- src/bataille/Application.java
+++ src/bataille/Application.java
@@ -11,17 +11,17 @@ public class Application {
     public static void main(String[] args) {
         Range battlefield= new Range (10,0,10,0);
         Coordonnees c= new Coordonnees(4,8);
-        Bateau bateau1 = new Bateau(c,Direction.Down, Modele.Small);
+        Bateau bateau1 = new Bateau(c,Direction.Down, Modele.Small, battlefield);
         System.out.println(bateau1.getDirection());
         System.out.println(bateau1.getModele());
         System.out.println((bateau1.getCoordonnees()));
 
-        Coordonnees d= new Coordonnees(8,4);
-        Bateau bateau2= new Bateau(d,Direction.Left,Modele.Large);
-        System.out.println(bateau2.getCoordonnees()); // Returns pointer, use "Arrays.toString() as previously to get value
+        Coordonnees d= new Coordonnees(8,8);
+        Bateau bateau2= new Bateau(d,Direction.Left,Modele.Large, battlefield);
         System.out.println(bateau2.getDirection());
         System.out.println(bateau2.getModele());
-    }
+        System.out.println(bateau2.getCoordonnees());
 
+    }
     }
 
--- src/bataille/Bateau.java
+++ src/bataille/Bateau.java
@@ -1,13 +1,18 @@
 package bataille;
-
-import java.util.ArrayList;
+import coordonnees.Range;
 import coordonnees.Coordonnees;
 import coordonnees.Direction;
 public class Bateau {
     private Coordonnees coordonnees;
     private Direction direction;
     private Modele modele;
-    public Bateau (Coordonnees coordonnees,Direction direction, Modele modele){
+    public Bateau (Coordonnees coordonnees,Direction direction, Modele modele, Range battlefield){
+       if (coordonnees.getX()>battlefield.getxMax()|| coordonnees.getX()<battlefield.getxMin()|| coordonnees.getY()<battlefield.getyMin()|| coordonnees.getY()>battlefield.getyMax()){
+           System.out.println("Start position NOPE");
+       }
+       else{
+           System.out.println("Start Position OK");
+       }
      this.coordonnees=coordonnees;
      this.modele=modele;
      this.direction=direction;
@@ -15,11 +20,9 @@ public class Bateau {
     public Direction getDirection() {
         return direction;
     }
-
     public Modele getModele() {
         return modele;
     }
-
     public Coordonnees getCoordonnees() {
         return coordonnees;
     }
